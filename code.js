7dcd8d43c5ae1f1a27855af3ac40193b871fad7f
Inti Luna <inlua@alumni.uv.es>
2025-05-28 16:53:21 +0200

    add lunch menu and display first plate option in console

--- code.js
+++ code.js
@@ -1,23 +1,38 @@
 // Code tested using codepen.io
+// usando de referencia menu: https://codebilbao.es/wp-content/uploads/2021/06/180621.pdf
 
 
-let menu_desayuno = {
-  1:{"tostadas": 2.5},
-  2:{"cereales": 3.0},
-  3:{"huevos": 4.0},
-  4:{"fruta": 1.5},
-}
 
-
-let menu_almuerzo = {
-  1:{"ensalada": 6.5},
-  2:{"hamburguesa": 8.0},
-  3:{"pasta": 7.0},
-  4:{"sopa": 5.0},
+let menuDesayuno = {
+  primerPlato:{1:{"tostadas": 2.5},
+               2:{"cereales": 3.0},
+               3:{"fruta": 1.5}},
+  segundoPlato:{1:{"huevos estrellados": 2.5},
+                2:{"huevos revueltos": 3.0},
+                3:{"yogurt y granola": 4.0}},
+  bebida:{1:{"cafe": 2.5},
+          2:{"leche": 3.0},
+          3:{"cafe con leche": 4.0},
+          4:{"zumo de fruta": 1.5}}
   
-}
+};
 
-let menu_cena = {
+
+let menuAlmuerzo = {
+  primerPlato:{ 1:{"Pisto a la bilbaína con bacalao desmigado": 8},
+                2:{"Ensalada cesar con aliño casero, pollo asado y picatostes": 6.0},
+                3:{"Ensalada de atun, tomate y huevo con rucula": 7.0},},
+  segundoPlato:{1:{"Lomo de lubina a la brasa con cous-cous especiado": 9.5},
+                2:{"Chuletas de pavo al ajillo con patatas fritas": 9.0},
+                3:{"Camarones al curry con arroz y verduras": 11.0}},
+  postre:{1:{"Brocheta de piña con crema de coco": 3.5},
+          2:{"Bizcocho con salsa tibia de chocolate": 3.0},
+          3:{"Pastel de zanahoria y nueces": 4.0},
+          }
+  
+};
+
+let menuCena = {
   1:{"pescado": 9.0},
   2:{"pollo": 8.5},
   3:{"verduras": 7.0},
@@ -36,18 +51,20 @@ try {
 
   let menuSeleccionado;
   if (hora > 6 && hora < 11) {
-    menuSeleccionado = menu_desayuno;
+    menuSeleccionado = menuDesayuno;
     console.log("Es desayuno");
   } else if (hora > 11 && hora < 15) {
-    menuSeleccionado = menu_almuerzo;
+    menuSeleccionado = menuAlmuerzo;
     console.log("Es almuerzo");
   } else if (hora > 18 && hora < 23) {
-    menuSeleccionado = menu_cena;
+    menuSeleccionado = menuCena;
     console.log("Es cena");
   }
 
   if (menuSeleccionado) {
     console.log("El menú actual es:", menuSeleccionado);
+    console.log("Opciones de primer plato son:", menuSeleccionado.primerPlato);
+    console.log("Primera opcion:", menuSeleccionado.primerPlato[1]);
   } else {
     console.log("Escribe otra hora, que la cocina está cerrada");
   }
